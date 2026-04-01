13090cfdbd4521798afbb5a8660f97f14f6bf6d6
Abhishek <abhiskul00@gmail.com>
2026-04-02 02:06:00 +0530

    implement the wss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { matchRouter } from './routes/matches.js';
+import { attachWebSocketServer } from './websocket.js';
+import http from 'http';
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
 
@@ -12,6 +17,11 @@ app.get('/', (req, res) => {
 
 app.use('/matches', matchRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const { broadcastMatchCreated } =  attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server.listen(PORT, HOST, () => {
+  const baseUrl = HOST === '0.0.0.0' ? `http://localhost${PORT}` : `http://${HOST}:${PORT}`;
+  console.log(`Server is running at ${baseUrl}`);
+  console.log(`WebSocket server is running at ${baseUrl.replace('http', 'ws')}/ws`);
 });
